add7237cd7ccaacad1741d9f2c737e3412c40b00
kingishksh <lazaromarcuslazaro@gmail.com>
2022-01-27 22:19:16 -0800

    updating HW and adding CarHW

---
+++ HW_CarsExceptionsCoreSol/CarsExceptionsCore/Program.cs
@@ -0,0 +1,85 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace CarsExceptionsCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(35);  // call constructor and set speed max to 35 (tiny engine!!)
+            string ErrorMessage = "All is well";
+
+            bool done = false; // loop control variable
+            while (!done)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("start = start engine, stop = stop engine");
+                Console.Write("accel = accelerate, brake = brake, q = quit. Command? :");
+                Console.ResetColor();
+                string userInput = Console.ReadLine();
+                userInput = userInput.ToLower();  // in case user happened to enter capital letters
+                Console.Clear();  // each time we get a new command, clear the prior history from the console
+                switch (userInput)
+                {
+                    case "start":
+                        try
+                        {
+                            myCar.StartEngine();
+                        }
+                        catch (ApplicationException e)
+                        {
+                            ErrorMessage = e.Message;
+                        }
+                        break;
+                    case "stop":
+                        try
+                        {
+                            myCar.StopEngine();
+                        }
+                        catch (ApplicationException e)
+                        {
+                            ErrorMessage = e.Message;
+                        }
+                        break;
+                    case "accel":
+                        try
+                        {
+                            myCar.Accelerate();
+                        }
+                        catch (ApplicationException e)
+                        {
+                            ErrorMessage = e.Message;
+                        }
+                        break;
+                    case "brake":
+                        myCar.Decelerate();
+                        break;
+                    case "q":
+                    case "e":
+                        Console.WriteLine("Goodbye");
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid input.");
+                        break;
+                }
+                // at the end of each command, call this method to show status
+                DisplayCarState(ErrorMessage, myCar);
+                ErrorMessage = "All is well";
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void DisplayCarState(string msg, Car myCar)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(msg);
+            Console.WriteLine("Engine is running: " + myCar.EngineRunning);
+            Console.WriteLine("Current speed is: " + myCar.CurrentSpeedMPH);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ HW_CarsExceptionsCoreSol/ClassLibrary1/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Car
+    {
+        // fields
+        private decimal _CurrentSpeedMPH; // the cars current speed
+        private decimal _MaxSpeedMPH;  // the upper limit, the car cannot exceed this value
+        private bool _EngineRunning;  // true means the engine has been started and is running, false says engine is off
+
+        public Car(decimal maxSpeed) // constuctor, user chooses to set maxSpeed as they create the object
+        {
+            _MaxSpeedMPH = maxSpeed;
+            _CurrentSpeedMPH = 0;
+            _EngineRunning = false;
+        }
+
+
+        public bool EngineRunning   // Property to allow reading the current value
+        { get { return _EngineRunning; } }
+
+        public decimal CurrentSpeedMPH    // Property to allow reading the current value
+        { get { return _CurrentSpeedMPH; } }
+
+
+
+        public void StartEngine()  // method used to start car's engine
+        {
+            if (_EngineRunning == true)
+            {
+                throw new ApplicationException("Engine is already started.");
+            }
+             _EngineRunning = true;
+        }
+
+        public void StopEngine()    // method used to stop car's engine
+        {
+            if (_EngineRunning == false)
+            {
+                throw new ApplicationException("Engine is not running");
+            }
+             _EngineRunning = false;
+        }
+
+
+        public void Accelerate()  // method used to increase the cars speed
+        {
+            if (_EngineRunning == false)
+            {
+                throw new ApplicationException("Please start engine.");
+            }
+                 _CurrentSpeedMPH = _CurrentSpeedMPH + 10;  // this method always tries to increase by 10 mph
+            if (_CurrentSpeedMPH > _MaxSpeedMPH)  // but it can never exceed the max speed as set in the constructor
+            {
+                _CurrentSpeedMPH = _MaxSpeedMPH;  // if speed is higher than max, slow it to max
+            }
+        }
+
+        public void Decelerate()  // this method always tries to decrease the speed by 5 mph
+        {
+            _CurrentSpeedMPH = _CurrentSpeedMPH - 5;  // but never lets it go below 0 and become negative.
+            if (_CurrentSpeedMPH < 0)
+            {
+                _CurrentSpeedMPH = 0;
+            }
+        }
+
+
+    }
+}
