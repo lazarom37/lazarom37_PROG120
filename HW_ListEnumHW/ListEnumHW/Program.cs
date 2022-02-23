47738fcfb31d397e843dcd3c02922785e451894e
kingishksh <lazaromarcuslazaro@gmail.com>
2022-02-22 19:50:12 -0800

    uploaded enums HW

---
+++ HW_ListEnumHW/ListEnumHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListEnumHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students[] newStudents = new Students[6];
+
+            newStudents[0] = new GradeSchool(Colors.blue, "Smith");
+            newStudents[1] = new GradeSchool(Colors.green, "Jones");
+            newStudents[2] = new HighSchool(Colors.red, "Lee");
+            newStudents[3] = new HighSchool(Colors.blue, "Yee");
+            newStudents[4] = new College(Colors.green, "Adelaida");
+            newStudents[5] = new College(Colors.red, "Rossi");
+            
+            List<Students> kids = new List<Students>();
+
+            for(int i = 0; i < newStudents.Length; i++)
+            {
+                kids.Add(newStudents[i]);
+            }
+
+            foreach (Students item in kids)
+            {
+                Console.WriteLine($"last name is {item.LastName}");
+            }
+
+            Console.WriteLine(); //spacer
+            
+            List<College> adults = new List<College>();
+            adults.Add((College)newStudents[4]);
+            adults.Add((College)newStudents[5]);
+
+            foreach (College item in adults)
+            {
+                Console.WriteLine($"last name is {item.LastName}, class size is {item.Size}, favorite color is {item.FavoriteColor}");
+            }
+            
+        }
+    }
+}
---
+++ HW_ListEnumHW/ListEnumHW/Students.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListEnumHW
+{
+    abstract class Students
+    {
+        private string _LastName;
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+
+    }
+
+    enum ClassSize
+    {
+        Large,
+        VeryLarge,
+        ExtremelyLarge
+    }
+
+    enum Colors
+    {
+        red,
+        blue,
+        green
+    }
+
+    class GradeSchool : Students
+    {
+        public ClassSize Size { get; set;}
+
+        public Colors FavoriteColor { get; set; }
+
+        public GradeSchool(Enum pColor, string pLastName)
+        {
+            FavoriteColor = (Colors)pColor;
+            LastName = pLastName;
+            Size = ClassSize.Large;
+        }
+    }
+
+    class HighSchool : Students
+    {
+        public ClassSize Size { get; set; }
+        public Colors FavoriteColor { get; set; }
+
+        public HighSchool(Enum pColor, string pLastName)
+        {
+            FavoriteColor = (Colors)pColor;
+            LastName = pLastName;
+            Size = ClassSize.VeryLarge;
+        }
+    }
+
+    class College : Students
+    {
+        public ClassSize Size { get; set; }
+        public Colors FavoriteColor { get; set; }
+
+        public College(Enum pColor, string pLastName)
+        {
+            FavoriteColor = (Colors)pColor;
+            LastName = pLastName;
+            Size = ClassSize.ExtremelyLarge;
+        }
+    }
+}
